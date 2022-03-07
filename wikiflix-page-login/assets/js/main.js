0d01b934c94680c91b5aac533f186b4d02a70e73
Leonardo Rodrigues <rodriguespeixoto.1995@gmail.com>
2022-03-07 20:01:16 -0300

    Atualização do repositorio

--- wikiflix-page-login/assets/js/main.js
+++ wikiflix-page-login/assets/js/main.js
@@ -1,39 +1,15 @@
 // show menu
 
-// Função para abrir pop-up de cadastro
-
-function iniciaModal(modalID) {
-  const modal = document.getElementById(modalID);
-  if (modal) {
-    modal.classList.add("mostrar");
-    modal.addEventListener("click", (e) => {
-      if (e.target.id == modalID || e.target.className == "close") {
-        modal.classList.remove("mostrar");
-      }
+const showMneu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+  /*valida se se existe as variaveis*/
+  if (toggle && nav) {
+    /*aqui adiciono a class show-menu na div com a class nav__menu*/
+    toggle.addEventListener("click", () => {
+      /*adicina a class show-menu na div que tem o id nav-menu */
+      nav.classList.toggle("show-menu");
     });
   }
-}
-
-const signUpModal = document.querySelector(".signUp");
-signUpModal.addEventListener("click", function () {
-  iniciaModal("modal-signUp");
-});
-
-// Função para abrir pop-up para recuperar senha
-
-function iniciaModal(modalID) {
-  const modal = document.getElementById(modalID);
-  if (modal) {
-    modal.classList.add("mostrar");
-    modal.addEventListener("click", (e) => {
-      if (e.target.id == modalID || e.target.className == "close") {
-        modal.classList.remove("mostrar");
-      }
-    });
-  }
-}
-
-const forgotPassword = document.querySelector(".forgot-password");
-forgotPassword.addEventListener("click", function () {
-  iniciaModal("modal-forgot");
-});
+};
+showMneu("nav-toggle", "nav-menu");
